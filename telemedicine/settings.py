57e580c92829d73db9e69bfca71456fb5315330a
oluwaseun <t.oluwaseunofficial@gmail.com>
2026-04-10 10:59:35 -0400

    added the REST_FRAMEWORK and SIMPLE_JWT

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -129,3 +129,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
