1e1da4f25886db187105e8519638a866f9a1f55c
oluwaseun <t.oluwaseunofficial@gmail.com>
2026-04-10 13:45:20 -0400

    implemented authentication with JWT

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    user = User.objects.create_user(
+        email=data['email'],
+        username=data['username'],
+        password=data['password']
+    )
+
+    return Response({"message": "User created successfully"})
