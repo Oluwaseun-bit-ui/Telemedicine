0f830cfc4aa2b2ac7d34d2d18e65daafc8b433c9
oluwaseun <t.oluwaseunofficial@gmail.com>
2026-04-14 13:02:19 -0400

    add user profile endpoint

--- telemedicine/urls.py
+++ telemedicine/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home 
+from telemedicine.views import home 
 
 urlpatterns = [
     path ('', home), 
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
+    path('api/users/', include('users.urls')),
 ]
